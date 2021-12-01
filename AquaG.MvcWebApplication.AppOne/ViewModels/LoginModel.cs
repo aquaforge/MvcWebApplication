57b8ed381d012356c1f52304483432d9c3b74d08
IvanGu <gusagusa@gmail.com>
2021-12-01 18:12:24 +0300

    косметика

--- AquaG.MvcWebApplication.AppOne/ViewModels/LoginModel.cs
+++ AquaG.MvcWebApplication.AppOne/ViewModels/LoginModel.cs
@@ -11,7 +11,6 @@ namespace AquaG.MvcWebApplication.AppOne.ViewModels
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Display(Name = "Запомнить?")]
         public bool RememberMe { get; set; }
         public string ReturnUrl { get; set; }
     }
