71240b27960f6333372f9582c6a198cff2857de1
IvanGu <gusagusa@gmail.com>
2021-12-01 16:38:47 +0300

    Добавьте файлы проекта.

---
+++ AquaG.MvcWebApplication.AppOne/Controllers/AccountController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using AquaG.MvcWebApplication.AppOne.Models;
+using AquaG.MvcWebApplication.AppOne.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+
+namespace AquaG.MvcWebApplication.AppOne.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserContext db;
+
+        public AccountController(UserContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "")
+        {
+            var model = new LoginModel { ReturnUrl = returnUrl };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+                if (user != null)
+                {
+                    await Authenticate(model.Email); // аутентификация
+
+                    return Redirect(GetLocalRedirectString(model.ReturnUrl));
+                }
+                ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            }
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public IActionResult Register(string returnUrl = "")
+        {
+            var model = new RegisterModel { ReturnUrl = returnUrl };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+                if (user == null)
+                {
+                    // добавляем пользователя в бд
+                    db.Users.Add(new User { Email = model.Email, Password = model.Password });
+                    await db.SaveChangesAsync();
+
+                    await Authenticate(model.Email); // аутентификация
+
+                    return Redirect(GetLocalRedirectString(model.ReturnUrl));
+                }
+                else
+                    ModelState.AddModelError("", "Пользователь уже существует");
+            }
+            return View(model);
+        }
+
+        private async Task Authenticate(string userName)
+        {
+            // создаем один claim
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+            };
+            // создаем объект ClaimsIdentity
+            ClaimsIdentity id = new(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            // установка аутентификационных куки
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        private string GetLocalRedirectString(string returnUrl)
+        {
+            if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                return returnUrl;
+            else
+                return "/";
+        }
+
+    }
+}
